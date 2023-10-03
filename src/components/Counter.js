4115c618815fc28da16b026f8880795e8029d8d1
MAKSIM <max_air@bk.ru>
2023-10-03 20:17:29 +0300

    useDispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,40 @@
-import { connect } from 'react-redux'
+///import { connect } from 'react-redux'
 import { inc, dec, rnd } from './actionCreators'
-const Counter = ({ counter, inc, dec, rnd }) => {
+//import { useSelector, useDispatch } from 'react-redux/es/hooks/useSelector';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+
+const Counter = () => {
+    const counter = useSelector(state => state.value);
+    const dispatch = useDispatch();
+
+
     return (
         <>
             <div >{counter}</div>
-            <button onClick={inc}>inc</button>
-            <button onClick={dec}>dec</button>
-            <button onClick={rnd}>rnd</button>
+            <button onClick={() => dispatch(inc())}>inc</button>
+            <button onClick={() => dispatch(dec())}>dec</button>
+            <button onClick={() => dispatch(rnd())}>rnd</button>
         </>
     )
 }
-const mapStateToProps = (state) => {
-    return {
-        counter: state.value
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        inc: () => dispatch(inc()),
-        dec: () => dispatch(dec()),
-        rnd: ()=> dispatch(rnd())
-    }
-}
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+// const mapStateToProps = (state) => {
+//     return {
+//         counter: state.value
+//     }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//     return {
+//         inc: () => dispatch(inc()),
+//         dec: () => dispatch(dec()),
+//         rnd: ()=> dispatch(rnd())
+//     }
+// }
+
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
